43d4ae75b3374bcc6a7a2dfa5c6f09afd8a283c6
jahangeercs123 <jahangeerahmad26@gmail.com>
2023-05-06 02:36:11 +0500

    this is the first version boat shipping

---
+++ js/custom.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+    $('.banner-inner').slick(
+        {
+            nextArrow: '<button class="my-arrow-class BannerbtnNext "><i class="fa fa-angle-right" ></i> </button>',
+            prevArrow: '<button  class="my-arrow-class BannerbtnPrev"><i class="fa fa-angle-left" ></i></button>'
+        }
+    );
+    $( ".icon-one" ).click(function(){
+        $(this).toggleClass('active-one');
+        $( ".nav-buttom nav ul" ).slideToggle();
+        
+    });
+
+  })
